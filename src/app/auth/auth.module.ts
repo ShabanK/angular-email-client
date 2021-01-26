156e9fc98b161409ee98b18b55607ee70d8e6037
ShabanK <shabankhawar99@gmail.com>
2021-01-26 20:00:25 +0500

    break time

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,14 @@ import {ReactiveFormsModule} from "@angular/forms"
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
+import {SharedModule} from "../shared/shared.module"
 
 @NgModule({
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   providers: [],
   declarations: [SigninComponent, SignupComponent]
