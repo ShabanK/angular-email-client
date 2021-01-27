db662fb26aa8b40d4e08957aaacad97fc4b621fe
ShabanK <shabankhawar99@gmail.com>
2021-01-27 18:37:15 +0500

    generated an interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {AuthModule} from "./auth/auth.module"
 import {AuthService} from "./auth/auth.service"
 import {MatchPassword} from "./auth/validators/match-password"
 import {UniqueUserName} from "./auth/validators/unique-user-name"
-
+import {AuthHttpInterceptor} from "./auth/auth-http-interceptor"
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import {UniqueUserName} from "./auth/validators/unique-user-name"
     AppRoutingModule,
     AuthModule, HttpClientModule
   ],
-  providers: [MatchPassword, UniqueUserName, AuthService],
+  providers: [MatchPassword, UniqueUserName, AuthService, AuthHttpInterceptor],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
