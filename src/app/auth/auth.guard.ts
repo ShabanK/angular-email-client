c648bf93d8ba310d8c0e4e5326a41cc566f798b7
ShabanK <shabankhawar99@gmail.com>
2021-01-29 18:01:24 +0500

    angular 5 redirects from guards are broken

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,22 @@
 import { Injectable } from "@angular/core";
-import { CanLoad, Route, UrlSegment } from "@angular/router";
+import { CanLoad, Route, Router } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { AuthService } from "./auth.service";
-import { skipWhile, take } from "rxjs/operators";
+import { skipWhile, take, tap } from "rxjs/operators";
 
 @Injectable()
 export class AuthGuard implements CanLoad {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.signedIn$.pipe(
       skipWhile((value) => value === null),
-      take(1)
+      take(1),
+      tap((authenticated) => {
+        if (!authenticated) {
+          this.router.navigateByUrl("/");
+        }
+      })
     );
   }
 }
