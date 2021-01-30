cfa49222d66675633e1a83e54f3f5dbc501b84b6
ShabanK <shabankhawar99@gmail.com>
2021-01-30 14:49:51 +0500

    route params

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { EmailService } from "../email.service";
 @Component({
-  selector: 'app-email-show',
-  templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  selector: "app-email-show",
+  templateUrl: "./email-show.component.html",
+  styleUrls: ["./email-show.component.css"],
 })
 export class EmailShowComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(({ id }) => {
+      console.log(id);
+    });
   }
-
 }
